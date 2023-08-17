163d6ea0ae8a9e33821dd2ce89612eab51e7eba2
aminDB81 <aminbaluchi67@gmail.com>
2023-08-18 01:34:41 +0330

    Add product details

--- JS/script.js
+++ JS/script.js
@@ -6,9 +6,48 @@ let body = document.querySelector('body');
 let total = document.querySelector('.total');
 let quantity = document.querySelector('.quantity');
 
-openShopping.addEventListener('click', ()=>{
+openShopping.addEventListener('click', () => {
     body.classList.add('active');
 })
-closeShopping.addEventListener('click', ()=>{
+closeShopping.addEventListener('click', () => {
     body.classList.remove('active');
-})
+});
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1',
+        image: '1.jpg',
+        price: 120 +"$"
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2',
+        image: '2.jpg',
+        price: 160 +"$"
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3',
+        image: '3.jpg',
+        price: 220 +"$"
+    },
+    {
+        id: 4,
+        name: 'PRODUCT NAME 4',
+        image: '4.jpg',
+        price: 190 +"$"
+    },
+    {
+        id: 5,
+        name: 'PRODUCT NAME 5',
+        image: '5.jpg',
+        price: 320 + "$"
+    },
+    {
+        id: 6,
+        name: 'PRODUCT NAME 6',
+        image: '6.jpg',
+        price: 400 + "$"
+    }
+];
