85fb44383d06270a45a012b96c10f802665ad596
aminDB81 <aminbaluchi67@gmail.com>
2023-08-18 02:34:55 +0330

    Changing styles and adding js codes

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 let openShopping = document.querySelector('.shopping');
-let closeShopping = document.querySelector('.closeshopping');
+let closeShopping = document.querySelector('.closeShopping');
 let list = document.querySelector('.list');
 let listCard = document.querySelector('.listCard');
 let body = document.querySelector('body');
@@ -16,38 +16,93 @@ closeShopping.addEventListener('click', () => {
 let products = [
     {
         id: 1,
-        name: 'PRODUCT NAME 1',
+        name: 'iMac 24-inch',
         image: '1.jpg',
-        price: 120 +"$"
+        price: 1199
     },
     {
         id: 2,
-        name: 'PRODUCT NAME 2',
+        name: 'Smart Watch',
         image: '2.jpg',
-        price: 160 +"$"
+        price: 645
     },
     {
         id: 3,
-        name: 'PRODUCT NAME 3',
+        name: 'Google Pixel 7',
         image: '3.jpg',
-        price: 220 +"$"
+        price: 750
     },
     {
         id: 4,
-        name: 'PRODUCT NAME 4',
+        name: 'LG Smart TV',
         image: '4.jpg',
-        price: 190 +"$"
+        price: 999 
     },
     {
         id: 5,
-        name: 'PRODUCT NAME 5',
+        name: 'Gaming PC',
         image: '5.jpg',
-        price: 320 + "$"
+        price: 1155 
     },
     {
         id: 6,
-        name: 'PRODUCT NAME 6',
+        name: 'ipad pro',
         image: '6.jpg',
-        price: 400 + "$"
+        price: 1099
     }
 ];
+let listCards  = [];
+function initApp(){
+    products.forEach((value, key) =>{
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('item');
+        newDiv.innerHTML = `
+            <img src="images/${value.image}">
+            <div class="title">${value.name}</div>
+            <div class="price">$${value.price.toLocaleString()}</div>
+            <button onclick="addToCard(${key})">Add To Card</button>`;
+        list.appendChild(newDiv);
+    })
+}
+initApp();
+function addToCard(key){
+    if(listCards[key] == null){
+        // copy product form list to list card
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1;
+    }
+    reloadCard();
+}
+function reloadCard(){
+    listCard.innerHTML = '';
+    let count = 0;
+    let totalPrice = 0;
+    listCards.forEach((value, key)=>{
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+        if(value != null){
+            let newDiv = document.createElement('li');
+            newDiv.innerHTML = `
+                <div><img src="images/${value.image}"/></div>
+                <div>${value.name}</div>
+                <div>$${value.price.toLocaleString()}</div>
+                <div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                    <div class="count">${value.quantity}</div>
+                    <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>`;
+                listCard.appendChild(newDiv);
+        }
+    })
+    total.innerText = `$${totalPrice.toLocaleString()}`;
+    quantity.innerText = count;
+}
+function changeQuantity(key, quantity){
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    reloadCard();
+}
